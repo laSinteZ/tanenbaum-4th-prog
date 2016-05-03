227cb4ca58a0320c326966cf92998d1e3fa2a2c9
lebdron <lebdron@users.noreply.github.com>
2016-05-03 23:08:18 +0300

    2.61 solution

--- chapter_2/261.c
+++ chapter_2/261.c
@@ -1,21 +1,49 @@
-/*assuming we have a concrete number of prcesses.
-id of every process is one of the nubers from 0 to number of processes.*/
-void processDoWork(int id, int n_pocesses, int *turn){
-	while(TRUE){
-		while(turn!=id)
-			critical_region();
-		if(id<n_pocesses)
-			(*turn) = id;
-		else 
-			(*turn) = 0;
-		noncritical_region();
-	}
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+
+#define N 3
+
+int *turn;
+
+void critical_region(int i)
+{
+    printf("Process %i is in its critical region\n", i);
+    sleep(1);
+    printf("Process %i left its critical region\n", i);
 }
 
-void critical_region(){
-	sleep(1);
+void noncritical_region(int i)
+{
+    printf("Process %i is in its noncritical region\n", i);
+    sleep(1);
+    printf("Process %i left its noncritical region\n", i);
 }
 
-void noncritical_region(){
-	sleep(10);
+void work(int i)
+{
+    while(1){
+        while(*turn != i);
+        critical_region(i);
+        *turn = (i + 1) % N;
+        noncritical_region(i);
+    }
+}
+
+int main(void)
+{
+    turn = mmap(turn, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    *turn = 0;
+    int i;
+    for(i = 0; i < N; ++i){
+        if(!fork()){
+            work(i);
+            exit(0);
+        }
+    }
+    while(wait(NULL) > 0);
+    munmap(turn, sizeof(int));
+    return 0;
 }
